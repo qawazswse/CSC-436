fa43e01a2cb3552a092d8c4329d94bacebb30472
qawazswse <31367515+qawazswse@users.noreply.github.com>
2020-11-02 00:52:41 -0600

    hw3 base on hw2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule} from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+import {LocationStrategy, HashLocationStrategy} from '@angular/common';
+
+const routes: Routes = [
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
+  bootstrap: [AppComponent],
+}
+)
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, Component } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import {TravelRequestComponent} from './travel_request/travelRequest.component';
-import {FormsModule} from '@angular/forms';
+import {AboutComponent} from './about/about.component';
+
+const routes: Routes = [
+  {path: 'travel_request', component: TravelRequestComponent},
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    TravelRequestComponent
+    TravelRequestComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
